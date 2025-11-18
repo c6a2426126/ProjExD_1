15f95d38855c7255d7f12c8d35c7930e2932c1a5
Yae <c0a2426126@edu.teu.ac.jp>
2025-11-18 17:11:22 +0900

    練習９：残像解消

--- pygame_image.py
+++ pygame_image.py
@@ -19,9 +19,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr
+        x = tmr % 3200 #3200を超えると0になる
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
